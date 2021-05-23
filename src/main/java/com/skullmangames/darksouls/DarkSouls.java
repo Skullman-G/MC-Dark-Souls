10566405534cb75ad98e55c69565486125c65e69
Skullman-G <64651571+Skullman-G@users.noreply.github.com>
2021-05-23 14:52:51 +0200

    estus flask added

--- src/main/java/com/skullmangames/darksouls/DarkSouls.java
+++ src/main/java/com/skullmangames/darksouls/DarkSouls.java
@@ -1,35 +1,27 @@
 package com.skullmangames.darksouls;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import com.skullmangames.darksouls.core.init.ItemInit;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("darksouls")
+@Mod(DarkSouls.MOD_ID)
 public class DarkSouls
 {
-    // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "darksouls"; 
 
-    public DarkSouls() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
+    public DarkSouls() 
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    	
+    	bus.addListener(this::setup);
+    	ItemInit.ITEMS.register(bus);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
--- src/main/java/com/skullmangames/darksouls/core/init/ItemInit.java
+++ src/main/java/com/skullmangames/darksouls/core/init/ItemInit.java
@@ -1,5 +1,16 @@
 package com.skullmangames.darksouls.core.init;
 
-public class ItemInit {
+import com.skullmangames.darksouls.DarkSouls;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit 
+{
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DarkSouls.MOD_ID);
+	
+	public static final RegistryObject<Item> ESTUS_FLASK = ITEMS.register("estus_flask", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_BREWING)));
 }
