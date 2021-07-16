ed2176c818f36ef26d7a8b6695bb2633ed0cfe8b
Skullman-G <64651571+Skullman-G@users.noreply.github.com>
2021-07-16 17:27:14 +0200

    Update ResizeTextButton.java

--- src/main/java/com/skullmangames/darksouls/client/gui/widget/ResizeTextButton.java
+++ src/main/java/com/skullmangames/darksouls/client/gui/widget/ResizeTextButton.java
@@ -18,7 +18,7 @@ public class ResizeTextButton extends Button
 	
 	public ResizeTextButton(int p_i232256_1_, int p_i232256_2_, int p_i232256_3_, int p_i232256_4_, ITextComponent p_i232256_5_, Button.IPressable p_i232256_6_, Button.ITooltip p_i232256_7_)
 	{
-	      super(p_i232256_1_, p_i232256_2_, p_i232256_3_, p_i232256_4_, p_i232256_5_, p_i232256_6_, p_i232256_7_);
+		super(p_i232256_1_, p_i232256_2_, p_i232256_3_, p_i232256_4_, p_i232256_5_, p_i232256_6_, p_i232256_7_);
 	}
 	
 	@Override
@@ -37,11 +37,9 @@ public class ResizeTextButton extends Button
 	    int j = getFGColor();
 	    
 	    matrixstack.pushPose();
-	    double xscale = 0.8D;
-	    double yscale = 0.8D;
-	    matrixstack.scale((float)xscale, (float)yscale, 0.0F);
-	    matrixstack.translate((1.0D - xscale) * 100 * 3, (1.0D - yscale) * 100 + 15, 0.0D);
-	    drawCenteredString(matrixstack, fontrenderer, this.getMessage(), this.x + this.width / 2, this.y + (this.height - 8) / 2, j | MathHelper.ceil(this.alpha * 255.0F) << 24);
+	    float scale = 0.8F;
+	    matrixstack.scale(scale, scale, scale);
+	    drawCenteredString(matrixstack, fontrenderer, this.getMessage(), Math.round((this.x + this.width / 2) / scale), Math.round((this.y + (this.height - 8) / 2) / scale), j | MathHelper.ceil(this.alpha * 255.0F) << 24);
 		matrixstack.popPose();
 	    
 		if (this.isHovered())
