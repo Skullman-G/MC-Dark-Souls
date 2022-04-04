bb7273c3a45dca4cf1f30afefb9f81a5c5626174
Skullman-G <skullmanslashgames@gmail.com>
2022-04-04 18:32:10 +0200

    readded fire keeper ruins

---
+++ src/main/java/com/skullmangames/darksouls/common/structures/FireKeeperRuins.java
@@ -0,0 +1,51 @@
+package com.skullmangames.darksouls.common.structures;
+
+import java.util.Optional;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
+import net.minecraft.world.level.levelgen.structure.PostPlacementProcessor;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGenerator;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+
+public class FireKeeperRuins extends StructureFeature<JigsawConfiguration>
+{
+	public FireKeeperRuins()
+	{
+		super(JigsawConfiguration.CODEC, FireKeeperRuins::createPiecesGenerator, PostPlacementProcessor.NONE);
+	}
+
+	@Override
+	public GenerationStep.Decoration step()
+	{
+		return GenerationStep.Decoration.SURFACE_STRUCTURES;
+	}
+
+	private static boolean isFeatureChunk(PieceGeneratorSupplier.Context<JigsawConfiguration> context)
+	{
+		return true;
+	}
+
+	public static Optional<PieceGenerator<JigsawConfiguration>> createPiecesGenerator(PieceGeneratorSupplier.Context<JigsawConfiguration> context)
+	{
+		if (!FireKeeperRuins.isFeatureChunk(context)) return Optional.empty();
+		
+		BlockPos blockpos = context.chunkPos().getMiddleBlockPosition(0);
+		int topLandY = context.chunkGenerator().getFirstFreeHeight(blockpos.getX(), blockpos.getZ(),
+				Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor());
+		blockpos = blockpos.above(topLandY);
+
+		Optional<PieceGenerator<JigsawConfiguration>> structurePiecesGenerator = JigsawPlacement.addPieces(context,
+				PoolElementStructurePiece::new,
+				blockpos,
+				false,
+				false
+		);
+		return structurePiecesGenerator;
+	}
+}
--- src/main/java/com/skullmangames/darksouls/core/init/ModStructures.java
+++ src/main/java/com/skullmangames/darksouls/core/init/ModStructures.java
@@ -1,33 +1,15 @@
 package com.skullmangames.darksouls.core.init;
 
-import java.util.function.Supplier;
-
 import com.skullmangames.darksouls.DarkSouls;
-import com.skullmangames.darksouls.common.world.structures.CheckpointPlainsStructure;
-import com.skullmangames.darksouls.common.world.structures.UndeadAsylumStructure;
-
-import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
+import com.skullmangames.darksouls.common.structures.FireKeeperRuins;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModStructures
 {
-	public static final DeferredRegister<StructureFeature<?>> STRUCTURES = DeferredRegister
-			.create(ForgeRegistries.STRUCTURE_FEATURES, DarkSouls.MOD_ID);
+	public static final DeferredRegister<StructureFeature<?>> STRUCTURES = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, DarkSouls.MOD_ID);
 
-	public static final RegistryObject<StructureFeature<JigsawConfiguration>> CHECKPOINT_PLAINS = register("checkpoint_plains", Decoration.SURFACE_STRUCTURES,
-			() -> new CheckpointPlainsStructure(JigsawConfiguration.CODEC));
-	public static final RegistryObject<StructureFeature<JigsawConfiguration>> UNDEAD_ASYLUM = register("undead_asylum", Decoration.SURFACE_STRUCTURES,
-			() -> new UndeadAsylumStructure(JigsawConfiguration.CODEC));
-	
-	private static <T extends FeatureConfiguration>RegistryObject<StructureFeature<T>> register(String name, Decoration deco,
-			Supplier<StructureFeature<T>> sup)
-	{
-		StructureFeature.STEP.put(sup.get(), deco);
-		return STRUCTURES.register(name, sup);
-	}
+	public static final RegistryObject<StructureFeature<?>> FIRE_KEEPER_RUINS = STRUCTURES.register("fire_keeper_ruins", FireKeeperRuins::new);
 }
