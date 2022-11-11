445d1f6748fb7ee50aeca7353226429e2e629d92
Skullman-G <skullmanslashgames@gmail.com>
2022-11-11 16:38:11 +0100

    added sunlight altar structure

--- src/main/java/com/skullmangames/darksouls/core/init/ModStructures.java
+++ src/main/java/com/skullmangames/darksouls/core/init/ModStructures.java
@@ -5,6 +5,8 @@ import com.skullmangames.darksouls.common.structures.FalconerCamp;
 import com.skullmangames.darksouls.common.structures.FireKeeperRuins;
 import com.skullmangames.darksouls.common.structures.FirelinkShrine;
 import com.skullmangames.darksouls.common.structures.LordranCamp;
+import com.skullmangames.darksouls.common.structures.SunlightAltar;
+
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,4 +21,5 @@ public class ModStructures
 	//public static final RegistryObject<StructureFeature<?>> UNDEAD_ASYLUM = STRUCTURES.register("undead_asylum", UndeadAsylum::new);
 	public static final RegistryObject<StructureFeature<?>> FIRELINK_SHRINE = STRUCTURES.register("firelink_shrine", FirelinkShrine::new);
 	public static final RegistryObject<StructureFeature<?>> FALCONER_CAMP = STRUCTURES.register("falconer_camp", FalconerCamp::new);
+	public static final RegistryObject<StructureFeature<?>> SUNLIGHT_ALTAR = STRUCTURES.register("sunlight_altar", SunlightAltar::new);
 }
