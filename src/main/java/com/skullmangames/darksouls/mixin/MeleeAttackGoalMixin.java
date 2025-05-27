c751562c07f2fbe0b84a49da05d4fb4bd4c0dc77
Skullman-G <skullmanslashgames@gmail.com>
2025-05-27 08:30:13 +0200

    fixed mixin shadow method

--- src/main/java/com/skullmangames/darksouls/mixin/MeleeAttackGoalMixin.java
+++ src/main/java/com/skullmangames/darksouls/mixin/MeleeAttackGoalMixin.java
@@ -7,26 +7,33 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 
 @Mixin(MeleeAttackGoal.class)
 public abstract class MeleeAttackGoalMixin
 {
-	@Shadow private int ticksUntilNextAttack;
-	
+	@Shadow
+	private int ticksUntilNextAttack;
+
+	@Shadow
+	protected abstract boolean requiresUpdateEveryTick();
+
+	private int adjustedTickDelay(int ticks)
+	{
+		return this.requiresUpdateEveryTick() ? ticks : Goal.reducedTickDelay(ticks);
+	}
+
 	@Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/world/entity/ai/goal/MeleeAttackGoal;resetAttackCooldown()V", cancellable = true)
 	protected void onResetAttackCooldown(CallbackInfo info)
 	{
 		info.cancel();
-		System.out.print("\nworks");
 		this.ticksUntilNextAttack = this.adjustedTickDelay(1000);
 	}
-	
+
 	@Inject(at = @At(value = "RETURN"), method = "Lnet/minecraft/world/entity/ai/goal/MeleeAttackGoal;getAttackInterval()I", cancellable = true)
 	protected void onGetAttackInterval(CallbackInfoReturnable<Integer> cir)
 	{
 		cir.setReturnValue(this.adjustedTickDelay(1000));
 	}
-	
-	/*@Shadow*/ protected abstract int adjustedTickDelay(int delay);
 }
